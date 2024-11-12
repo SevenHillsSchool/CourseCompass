bac51be97b8e857ae8b80393c1f82a2a056b25cc
Adi Duggal <123963842+TheOddAdi8@users.noreply.github.com>
2024-11-12 11:08:48 -0500

    Update NavigationBar.js

--- UIUX/NavigationBar.js
+++ UIUX/NavigationBar.js
@@ -6,6 +6,14 @@ function NavigationBar() {
       <div className="Nav">
         <header className="Nav-header">
           <img src={logo} className="Nav-logo" alt="logo" />
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
         </header>
       </div>
     );
